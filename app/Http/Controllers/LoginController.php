8ea36c2d830b4723201e0e20c91e98fed39b5262
rafiqzayana <69287178+rafiqzayana@users.noreply.github.com>
2022-07-01 08:55:31 +0700

    Update wip

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -20,10 +20,11 @@ class LoginController extends Controller
 
     public function postLogin(Request $request){
         //Authentification is user and password are correct or not
+        // dd(request()->all());
         if(Auth::attempt($request->only('username', 'password'))){
             $request->session()->regenerate();
             $data = $request->input();
-
+            // Hash::request()->password;
             $username = Auth::User()->username;
             $comp_name = Auth::User()->comp_name;
             $comp_code = Auth::User()->comp_code;
@@ -31,7 +32,7 @@ class LoginController extends Controller
             $request->session()->put('username', $username);
             $request->session()->put('comp_code', $comp_code);
             // dd(session()->all());
-
+            
             $current_date_time = Carbon::now()->toDateTimeString();
             DB::table('userlog')->insert(['username' =>Auth::user()->name, 'tbl'=>'ONLINE', 'idtbl'=> '0', 'notbl'=>'', 'act'=>'LOGIN', 'comp_code'=>$comp_code, 'usin'=>1,'datein'=>$current_date_time]);
             
