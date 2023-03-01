a4dc863b0dbf5ed85c3bad8fc0f9f7a7703e98ab
librankaio <rafiqzayana@gmail.com>
2023-03-01 12:56:40 +0700

    update local pemasukan

--- resources/views/print/excel/pemasukkan_report.blade.php
+++ resources/views/print/excel/pemasukkan_report.blade.php
@@ -100,7 +100,7 @@
                     @if ($item->jumlah == 0)
                     <td class="border-2">0</td>
                     @else
-                    <td class="border-2">{{ $item->jumlah }}</td>
+                    <td class="border-2">{{ number_format($item->jumlah, 2, '.', ',')}}</td>
                     @endif
                     @if ($item->nilai_barang == 0)
                     <td class="border-2">0</td>
