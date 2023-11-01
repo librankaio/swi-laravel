46c5fd404e5c2df74869bc1464115926d8759c9a
librankaio <95515543+librankaio@users.noreply.github.com>
2023-11-01 14:07:37 +0700

    update navbar profile pict 01112023

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -15,7 +15,8 @@
     @php
     $imgname = session('comp_code');
     @endphp
-    <img src='/img/{{ $imgname }}.png' alt="" class="profile_img"></img>
+    <img src='{{ asset('../img/{{ $imgname }}.png') }}' alt="" class="profile_img"></img>
+    {{-- <img src='/img/{{ $imgname }}.png' alt="" class="profile_img"></img> --}}
     <h4>{{ session('comp_name') }}</h4>
   </center>
   <a href="{{ 'pemasukan' }}" class="btn_Nav {{ 'pemasukan' == request()->path() ? 'btn_NavActive' : '' }}"><i
